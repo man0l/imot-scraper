57a9667f42039ed2af0c096cdd2bf994b5e1660c
Manol Trendafilov <manol@passion4.tech>
2023-06-23 13:33:48 +0300

    parse env variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,9 @@
 const dotenv = require("dotenv");
-dotenv.config();
+const path = require("path");
+dotenv.config({
+  path: path.join(__dirname, "../..", ".env")
+});
+
 module.exports = {
   rabbitmq: {
     host: process.env.RABBITMQ_HOST,
@@ -10,12 +14,15 @@ module.exports = {
     queue_property_listings: process.env.RABBITMQ_QUEUE_PROPERTY_LISTINGS
   },
   database: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
   },
   puppeteer: {
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    userDataDir: process.env.PUPPETEER_USER_DATA_DIR,
+    profileName: process.env.PUPPETEER_PROFILE_NAME,
+    waitTimeout: process.env.PUPPETEER_WAIT_TIMEOUT
   }
 };
