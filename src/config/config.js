0b000f57c278bb6285da7d5593c2f8f8abf8e16a
Manol Trendafilov <manol@passion4.tech>
2023-06-26 10:43:45 +0300

    add configurations

--- src/config/config.js
+++ src/config/config.js
@@ -17,7 +17,8 @@ module.exports = {
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   },
   puppeteer: {
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
