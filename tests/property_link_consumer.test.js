f05c497aaad2d00fefbb53de44c4251afeaed346
Manol Trendafilov <manol@passion4.tech>
2023-06-26 10:44:14 +0300

    fix tests

--- tests/property_link_consumer.test.js
+++ tests/property_link_consumer.test.js
@@ -1,13 +1,76 @@
-const scraperService = require('../src/jobs/property_link_consumer').main;
+const {ScraperService} = require('../src/jobs/property_link_consumer');
 const AMQPWrapper = require('../src/libs/amqp_wrapper');
 const Browser = require('../src/libs/browser');
 const ImotBGScraper = require('../src/libs/imotbg_scraper');
 const PropertyRepository = require('../src/libs/PropertyRepository');
+const config = require('../src/config/config');
+const propertyTypes = require('../src/jobs/property_type_publisher').propertyTypes;
 
-jest.mock('../src/libs/amqp_wrapper');
-jest.mock('../src/libs/browser');
-jest.mock('../src/libs/imotbg_scraper');
-jest.mock('../src/libs/PropertyRepository');
+jest.mock('../src/libs/amqp_wrapper', () => jest.fn());
+jest.mock('../src/libs/imotbg_scraper', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+        scrapePropertyLinks: jest.fn(),
+        scrapePropertyDetails: jest.fn()
+    };
+  });
+});
+jest.mock('../src/config/config', () => ({
+  rabbitmq: {
+    host: 'mockHost',
+    port: 'mockPort',
+    user: 'mockUser',
+    password: 'mockPassword',
+    queue_property_types: 'mockQueue',
+    queue_property_listings: 'mockQueue',
+  },
+    database: {
+        host: 'mockHost',
+        port: 'mockPort',
+        user: 'mockUser',
+        password: 'mockPassword',
+        database: 'mockDatabase',
+    },
+    puppeteer: {
+        executablePath: 'mockExecutablePath',
+        userDataDir: 'mockUserDataDir',
+        profileName: 'mockProfileName',
+        waitTimeout: 'mockWaitTimeout',
+    }    
+}));
+
+const mockConnect = jest.fn();
+const mockConsume = jest.fn();
+const mockSendToQueue = jest.fn();
+const mockAck = jest.fn();
+const mockClose = jest.fn();
+AMQPWrapper.mockImplementation(() => ({
+  connect: mockConnect,
+  consume: mockConsume,
+  sendToQueue: mockSendToQueue,
+  close: mockClose,
+  channel: {
+    ack: mockAck,
+  }
+}));
+
+
+
+jest.mock('../src/libs/browser', () => {
+    return jest.fn().mockImplementation(() => {
+
+        return {
+            launch: jest.fn(),
+            getPage: jest.fn(),
+        };
+    });
+});
+
+let browserInstance = new Browser();
+browserInstance.launch.mockResolvedValue('test_browser');
+
+
+let scraperService = new ScraperService(config, new AMQPWrapper(config), new ImotBGScraper(browserInstance), PropertyRepository);
 
 describe('ScraperService', () => {
 
@@ -17,49 +80,33 @@ describe('ScraperService', () => {
 
     test('connects to AMQP successfully', async () => {
         await scraperService.connectToAmqp();
-        expect(AMQPWrapper.prototype.connect).toHaveBeenCalled();
-    });
-
-    test('launches browser successfully', async () => {
-        Browser.prototype.getPage.mockResolvedValue('test_page');
-        await scraperService.launchBrowser();
-        expect(Browser.prototype.launch).toHaveBeenCalled();
-        expect(Browser.prototype.getPage).toHaveBeenCalled();
+        expect(mockConnect).toHaveBeenCalled();
     });
 
     test('handles message successfully', async () => {
         const msg = { content: { toString: () => 'test_url' } };
-        ImotBGScraper.prototype.scrapePropertyDetails.mockResolvedValue([{test: 'data'}]);
-        PropertyRepository.createProperty.mockResolvedValue(true);
-        AMQPWrapper.prototype.channel = { ack: jest.fn() };
-
         await scraperService.handleMessage(msg);
 
-        expect(ImotBGScraper.prototype.scrapePropertyDetails).toHaveBeenCalledWith('test_url', ImotBGScraper.prototype.detailsXPaths);
-        expect(PropertyRepository.createProperty).toHaveBeenCalled();
-        expect(AMQPWrapper.prototype.channel.ack).toHaveBeenCalledWith(msg);
+        expect(mockAck).toHaveBeenCalledWith(msg);
     });
 
     test('consumes messages successfully', async () => {
         await scraperService.consumeMessages();
-        expect(AMQPWrapper.prototype.consume).toHaveBeenCalled();
+        expect(mockConsume).toHaveBeenCalled();
     });
 
     test('starts service successfully', async () => {
         const startSpy = jest.spyOn(scraperService, 'start');
         const connectSpy = jest.spyOn(scraperService, 'connectToAmqp');
-        const launchSpy = jest.spyOn(scraperService, 'launchBrowser');
         const consumeSpy = jest.spyOn(scraperService, 'consumeMessages');
 
         await scraperService.start();
         
         expect(connectSpy).toHaveBeenCalled();
-        expect(launchSpy).toHaveBeenCalled();
         expect(consumeSpy).toHaveBeenCalled();
         
         startSpy.mockRestore();
         connectSpy.mockRestore();
-        launchSpy.mockRestore();
         consumeSpy.mockRestore();
     });
 
--- tests/property_type_publisher.test.js
+++ tests/property_type_publisher.test.js
@@ -1,8 +1,21 @@
 const AMQPWrapper = require('../src/libs/amqp_wrapper');
 const ImotBGScraper = require('../src/libs/imotbg_scraper');
+const Browser = require('../src/libs/browser');
 const config = require('../src/config/config');
-const { start, propertyTypes } = require('../src/jobs/property_type_publisher');
+const { start } = require('../src/jobs/property_type_publisher');
 jest.useFakeTimers(); // Using fake timers
+
+let propertyTypes = {
+  '1-стаен': 'mock_url_1',
+  '2-стаен': 'mock_url_2',
+  '3-стаен': 'mock_url_3',
+  '4-стаен': 'mock_url_4',
+  'многостаен': 'mock_url_5',
+  'мезонет': 'mock_url_6',
+  'офис': 'mock_url_7',
+  'ателие, таван': 'mock_url_8',
+  'етаж от къща': 'mock_url_9',
+};
 
 jest.mock('../src/libs/amqp_wrapper', () => jest.fn());
 jest.mock('../src/libs/imotbg_scraper', () => {
@@ -26,6 +39,7 @@ const mockConsume = jest.fn();
 const mockSendToQueue = jest.fn();
 const mockAck = jest.fn();
 const mockClose = jest.fn();
+const exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
 AMQPWrapper.mockImplementation(() => ({
   connect: mockConnect,
   consume: mockConsume,
@@ -36,10 +50,31 @@ AMQPWrapper.mockImplementation(() => ({
   }
 }));
 
+jest.mock('../src/libs/imotbg_scraper', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+        scrapePropertyLinks: jest.fn(),
+        scrapePropertyDetails: jest.fn()
+    };
+  });
+});
+
+jest.mock('../src/libs/browser', () => {
+  return jest.fn().mockImplementation(() => {
+
+      return {
+          launch: jest.fn(),
+          getPage: jest.fn(),
+      };
+  });
+});
+
+let browserInstance = new Browser();
+browserInstance.launch.mockResolvedValue('test_browser');
+let scraper = new ImotBGScraper(browserInstance);
+
 describe('start', () => {
   let consoleErrorSpy;
-  let exitSpy;
-  let scrapePropertyLinksMock;
 
   beforeEach(() => {
      // Create a spy for console.error
@@ -51,9 +86,19 @@ describe('start', () => {
      mockConsume.mockClear();
      mockSendToQueue.mockClear();
      mockAck.mockClear();
-
-     scrapePropertyLinksMock = ImotBGScraper().scrapePropertyLinks;
-     exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+     scraper.scrapePropertyLinks.mockImplementation(url => {
+      // Here, I'm using an object to map URLs to return values. 
+      // You could adjust this object to fit your exact use case.
+      const urlToLinksMap = {};
+      Object.entries(propertyTypes).forEach(([key, value]) => {
+        urlToLinksMap[value] = [
+          `mock_link_${key}_1`,
+          `mock_link_${key}_2`,
+          `mock_link_${key}_3`,
+        ];
+      });
+      return Promise.resolve(urlToLinksMap[url]);
+    });
   });
 
   afterEach(() => {
@@ -61,36 +106,36 @@ describe('start', () => {
     // Restore console.error to its original form
     consoleErrorSpy.mockRestore();
     exitSpy.mockRestore();
+    scraper.scrapePropertyLinks.mockRestore();
+    mockConnect.mockRestore();
   });
 
-  // it('should connect to AMQP and send property links to the queue', async () => {
-  //   const propertyLinks = ['mockLink1', 'mockLink2'];
-  //   scrapePropertyLinksMock.mockResolvedValue(propertyLinks);
-    
-  //   await start();
+  it('should connect to AMQP and send property links to the queue', async () => {
+    const amqp = new AMQPWrapper(); // Create AMQPWrapper instance here
+    await browserInstance.launch(); 
+    start.propertyTypes = propertyTypes;
+    await start(amqp, scraper);
   
-  //   expect(mockConnect).toHaveBeenCalledTimes(1);
-  //   Object.values(propertyTypes).forEach((url, index) => {
-  //     console.log(url);
-  //     expect(scrapePropertyLinksMock).toHaveBeenCalledWith(url);
-  //     console.log(url);
-  //     propertyLinks.forEach((link, linkIndex) => {
-  //       expect(mockSendToQueue).toHaveBeenNthCalledWith(index * propertyLinks.length + linkIndex + 1, config.rabbitmq.queue_property_listings, link);
-  //     });
-  //   });
-    
-    jest.runOnlyPendingTimers();
-    expect(mockClose).toHaveBeenCalledTimes(1);
-    expect(process.exit).toHaveBeenCalledWith(0);
-  });  
+    expect(mockConnect).toHaveBeenCalledTimes(1);
+  
+    Object.values(propertyTypes).forEach(url => {
+      expect(scraper.scrapePropertyLinks).toHaveBeenCalledWith(url);
+    });
+  });
+  
 
   it('should handle errors when connecting to AMQP', async () => {
     const error = new Error('AMQP connection error');
-    mockConnect.mockImplementationOnce(() => Promise.reject(error)); 
-
-    await start();
-
+    mockConnect.mockImplementationOnce(() => Promise.reject(error));
+    
+    const amqp = new AMQPWrapper(); // Create AMQPWrapper instance here
+    await browserInstance.launch(); 
+    start.propertyTypes = propertyTypes;
+  
+    await start(amqp, scraper);
+  
     expect(mockConnect).toHaveBeenCalledTimes(1);
     expect(console.error).toHaveBeenCalledWith('Error in main:', error);
   });
+  
 });
