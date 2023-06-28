75cfcb7ccf5d4dc5eab6a01de9f2a08ef27b36ee
Manol Trendafilov <manol@passion4.tech>
2023-06-28 21:25:51 +0300

    add jypiter notebook

--- src/jobs/extract_entities.js
+++ src/jobs/extract_entities.js
@@ -1,6 +1,8 @@
 const { Configuration, OpenAIApi } = require("openai");
 const {Property, PropertyType} = require('../models');
-
+const levenshtein = require('fast-levenshtein');
+const fs = require('fs').promises;
+const path = require('path');
 require("dotenv").config();
 
 
@@ -10,13 +12,27 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 let main = async () => {
+    let filePath = path.join(__dirname, '../' + 'config/sofia_streets.json');
+    let file = await fs.readFile(filePath, 'utf8');
+    let streets = JSON.parse(file);
+
 // get the last 10 properties
 const properties = await Property.findAll({
-    where: { propertyTypeId: [1,2,3,4,5,6,7] },
+    select: ['id', 'description', 'price', 'area', 'floor', 'year', 'constructionType', 'propertyFeatures', 'location', 'city', 'district'],
+    where: { propertyTypeId: [1,2,3,4,5,6,7, 8] },
     limit: 100,
-    order: [['createdAt', 'ASC']]
+    order: [['createdAt', 'DESC']]
 });
 
+for (property of properties) {
+    await sleep(3000);
+    //let streetName = findStreetInDescription(streets, property.description);
+    //console.log(streetName);
+    await query({"inputs": property.description}).then((response) => {
+        console.log(JSON.stringify(response));
+    });
+}
+return;
 properties.forEach(async property => {
             await sleep(3000);
             let prompt = `
@@ -54,6 +70,8 @@ properties.forEach(async property => {
             - Walking distance to the nearest object / street / transport stop or other entity? (string in meters if present)
             Output in JSON format with short keys and short values wth strings only if any of the above is present in the text above:
             `;
+            console.log(prompt);
+            return;
             const response = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: [{role: 'user', 'content': prompt}]
@@ -67,6 +85,32 @@ properties.forEach(async property => {
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/Davlan/bert-base-multilingual-cased-ner-hrl",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+function findStreetInDescription(streets, description) {
+    const words = description.split(/\W+/); // split the description into words
+    for (let i = 0; i < streets.length; i++) {
+      for (let j = 0; j < words.length; j++) {
+        if (levenshtein.get(streets[i], words[j]) <= 2) { // if Levenshtein distance is less than or equal to 2
+          return streets[i]; // return the matching street
+        }
+      }
+    }
+    return null; // return null if no street is found
+  }
+
 
 
 (async () => {
--- src/jobs/property_type_publisher.js
+++ src/jobs/property_type_publisher.js
@@ -3,6 +3,7 @@ const config = require('../config/config');
 const BrowserClass = require('../libs/browser');
 const ImotBGScraper = require('../libs/imotbg_scraper');
 const fs = require('fs');
+const {Property} = require('../models');
 
 const propertyTypes = {
   '1-стаен': 'https://imoti-sofia.imot.bg/pcgi/imot.cgi?act=11&f1=4&f2=1&f3=1&f4=%E3%F0%E0%E4%20%D1%EE%F4%E8%FF&f5=',
@@ -27,6 +28,8 @@ for (let name in propertyTypes) {
   types.push({ name: name, link: propertyTypes[name] });
 }
 
+const EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 3; // 3 days
+
 async function start (amqp, scraper) {
 try {
   for(i in types) {
@@ -38,6 +41,12 @@ try {
     propertyLinks = [...new Set(propertyLinks)]; // deduplication
 
     propertyLinks.forEach(async link => {
+      
+      let property = await Property.findOne({ where: { url: link } });
+      if (property && !didPropertyExpired(property)) {
+        return;
+      }
+      
       let msg = JSON.stringify({propType: types[i].name, link: link});
       await amqp.sendToQueue(config.rabbitmq.queue_property_listings, msg);
       console.log(msg + ' sent to queue');
@@ -47,6 +56,16 @@ try {
 } catch(e) {
   console.log("Error in start function: " + e.message);
 }
+
+}
+
+let didPropertyExpired = (property) => {
+  
+  if (property.updatedAt > Date.now() - EXPIRATION_TIME) {
+    return false;
+  }
+
+  return true;
 
 }
  
